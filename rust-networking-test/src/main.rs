417ed6320d80bfcddaef782d750e5ba1553c8241
Bradley Ngan <64651152+BNganB@users.noreply.github.com>
2023-05-28 09:42:58 +0100

    removed cli, added web gui

--- rust-networking-test/src/main.rs
+++ rust-networking-test/src/main.rs
@@ -1,102 +1,58 @@
-use std::io::{self, Write};
-use colored::*;
-use std::process::Command;
+#[macro_use] extern crate rocket;
 
-fn main() {
-    get_user_creds();
-    make_connection(get_connection_info());
+#[cfg(test)] mod tests;
+
+use rocket::{State, Shutdown};
+use rocket::fs::{relative, FileServer};
+use rocket::form::Form;
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select;
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
 }
 
-struct User {
-    user_name: String,
-    colour: (u8, u8, u8),
+/// Returns an infinite stream of server-sent events. Each event is a message
+/// pulled from a broadcast queue sent by the `post` handler.
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
 }
 
-struct Connection {
-    ip: String,
-    port: usize,
+/// Receive a message from a form submission and broadcast it to any receivers.
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send 'fails' if there are no active subscribers. That's okay.
+    let _res = queue.send(form.into_inner());
 }
 
-fn get_user_creds() {
-    let mut s = String::new();
-    print!("Please enter your username: ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    io::stdin()
-        .read_line(&mut s)
-        .expect("Failed to read input");
-
-    let mut r = String::new();
-    let mut g = String::new();
-    let mut b = String::new();
-
-    print!("User colour setup (RGB) - R: ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    io::stdin()
-        .read_line(&mut r)
-        .expect("Failed to read input");
-    let processed_r: u8 = r.trim().parse::<u8>().expect("Failed to parse R");
-
-    print!("User colour setup (RGB) - G: ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    io::stdin()
-        .read_line(&mut g)
-        .expect("Failed to read input");
-    let processed_g: u8 = g.trim().parse::<u8>().expect("Failed to parse G");
-
-    print!("User colour setup (RGB) - B: ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    io::stdin()
-        .read_line(&mut b)
-        .expect("Failed to read input");
-    let processed_b: u8 = b.trim().parse::<u8>().expect("Failed to parse B");
-
-    let current_user = User {
-        user_name: s.trim().to_string(),
-        colour: (processed_r, processed_g, processed_b),
-    };
-    println!(
-        "Welcome {}!",
-        current_user
-            .user_name
-            .truecolor(current_user.colour.0, current_user.colour.1, current_user.colour.2)
-    );
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("static")))
 }
-
-pub fn get_connection_info() -> (String, usize) {
-    let mut ip = String::new();
-    print!("Please enter the server ip: ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    io::stdin()
-        .read_line(&mut ip)
-        .expect("Failed to read input");
-
-    let mut port = String::new();
-    print!("Port number: ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    io::stdin()
-        .read_line(&mut port)
-        .expect("Failed to read input");
-
-    let port = port.trim().parse::<usize>().expect("Failed to parse port");
-
-    (ip.trim().to_string(), port)
-}
-
-fn make_connection((ip, port): (String, usize)) {
-    let current_connection = Connection { ip, port };
-    let command = format!("nc localhost {}", current_connection.port);  //change from localhost later
-
-    println!("Establishing connection...");
-
-    
-
-    let output = Command::new("sh")
-    .arg("-c")
-    .arg(command)
-    .output()
-    .expect("Failed to connect");
-
-    io::stdout().write_all(&output.stdout).unwrap()
-}
-
-
